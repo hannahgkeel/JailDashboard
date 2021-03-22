3312870cd1b75e08216dc01cfded6d2b44fd196c
unknown <hannahgracekeel@gmail.com>
2021-03-22 15:22:54 -0400

    added a header

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Footer from './components/Footer.js';
+import Header from './components/Header.js';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <h1>Justice System Software!</h1>
       <h2>Welcome to the JailDashboard</h2>
       <Footer />
