646ddfe668821ca01477512781cba7070d479a7e
Julian Osorio <julianoso9991@gmail.com>
2021-03-22 14:11:47 -0400

    Created footer.

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import Footer from './components/Footer.js';
+
 export default function App() {
   return (
     <div>
       <h1>Justice System Software!</h1>
       <h2>Welcome to the JailDashboard</h2>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from '@material-ui/core/Container';
+
+import '../styles/Footer.css'
+
+export default function Footer() {
+    return (
+        <Container className="Footer-container" maxWidth="sm">
+            <p>© 2021 COMP523 - Team A</p>
+        </Container>
+    );
+}
