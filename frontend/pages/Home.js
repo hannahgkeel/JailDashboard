4753e0abc6ea8ea792a203ec2afd0e256f033631
unknown <hannahgracekeel@gmail.com>
2021-03-23 12:44:06 -0400

    added a starting point for the search

--- frontend/pages/Home.js
+++ frontend/pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Footer from '../components/Footer.js';
 import Header from '../components/Header.js';
+import Search from '../components/Search.js';
 import "../App.css";
-//import "./Home.css";
+import "../styles/Home.css";
+import { Typography } from "@material-ui/core";
 
 
 
@@ -11,6 +13,12 @@ class Home extends React.Component {
         return (
             <div>
                 <Header />
+                <div className="home-header pure-g">
+                    <h2>
+                        <Typography variant="h6">Search by your county name</Typography>
+                        <Search />
+                        </h2>
+                </div>
                 <Footer />
             </div>
         )
