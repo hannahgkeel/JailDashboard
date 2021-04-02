6ea366f3d543ea59d503cabb2c3e58e83764f5a8
Julian Osorio <julianoso9991@gmail.com>
2021-04-02 14:40:23 -0400

    Search modified to include checkboxes for All detainees or Pretrial detainees choice

--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -3,23 +3,39 @@ import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import '../styles/Search.css';
 import { Typography, Button } from "@material-ui/core";
+import FormGroup from '@material-ui/core/FormGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 export default function Search() {
 
     // Initialize state
-    const [county, setCounty] = useState('');
+    const [state, setState] = useState({
+        county: '',
+        all: false,
+        pretrial: false,
+    });
 
-    function handleChange(event, value) {
-        setCounty(value);
+    const handleChange = (event, value) => {
+        setState({...state, county: value});
     }
-    function handleSubmit(event) {
+
+    const handleCheckboxChange = (event) => {
+        if (event.target.name === 'all') {
+            setState({...state, all: true, pretrial: false})
+        } else {
+            setState({...state, all:false, pretrial: true})
+        }
+    }
+
+    const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(county);
+        console.log("Submit button pressed.");
     }
 
     return (
         <div className='search-div pure-u-11-12 pure-u-md-1-2 pure-g'>
-            <Typography variant="h6">Search by your county name</Typography>
+            <Typography variant="h6">Search by county name</Typography>
             <div className="search-input pure-u-1 pure-u-md-2-3">
                 <form onSubmit={handleSubmit} style={{width: 300}}>
                     <Autocomplete
@@ -39,6 +55,17 @@ export default function Search() {
                             />
                         )}
                     />
+                    <FormGroup>
+                        <FormControlLabel
+                            control={<Checkbox checked={state.all} onChange={handleCheckboxChange} name="all" /> }
+                            label='All Detainees'
+                        />
+                        <FormControlLabel
+                            control={<Checkbox checked={state.pretrial} onChange={handleCheckboxChange} name="pretrial" /> }
+                            label='Pretrial Detainees'
+                        />
+                    </FormGroup>
+
                     <Button variant="contained" color="primary" size="small">Search</Button>
                 </form>
             </div>
