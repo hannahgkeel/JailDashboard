25024ddd16a437d3b789a0dc241eaab526955cba
unknown <hannahgracekeel@gmail.com>
2021-04-27 13:39:16 -0400

    testing search renders

--- frontend/App.test.js
+++ frontend/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Search from './components/Search'
 
-test('renders learn react link', () => {
+test('renders search component', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(<Search />);
   expect(linkElement).toBeInTheDocument();
 });
